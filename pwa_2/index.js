85238c3d4d63a7b29d995b4fca145b47a914bd94
harin49 <harinarayanan4057@gmail.com>
2024-08-28 15:52:54 +0530

    Test commit to check multiple gitconfigs

--- pwa_2/index.js
+++ pwa_2/index.js
@@ -1,23 +1,23 @@
-
-if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register("/serviceWorker_2.js").then(() => console.log('SW_2 registered!'))
-        .catch(err => console.log('Boo! service worker 2', err));
-} else{
-    console.log("serviceWorker not supported")
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/serviceWorker_2.js")
+    .then(() => console.log("SW_2 registered!"))
+    .catch((err) => console.log("Boo! service worker 2", err));
+} else {
+  console.log("serviceWorker is not supported");
 }
 
-const addImage = () =>{
-    const img = document.createElement("img");
-    img.src="/assets/happy.svg"
-    img.width=500;
-    img.height=600;
-    const container = document.getElementById("c");
-    console.log("container", container);
-    container.appendChild(img);
-    console.log("appended")
-}
+const addImage = () => {
+  const img = document.createElement("img");
+  img.src = "/assets/happy.svg";
+  img.width = 500;
+  img.height = 600;
+  const container = document.getElementById("c");
+  console.log("container", container);
+  container.appendChild(img);
+  console.log("appended");
+};
 
-
-setTimeout(()=>{
-    addImage();
+setTimeout(() => {
+  addImage();
 }, 1000);
